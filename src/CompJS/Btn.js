f674a37e5bf9f0d5c585025afb62436804e0982a
Aguadebebe <81484212+Aguadebebe@users.noreply.github.com>
2024-12-29 03:11:14 -0500

    Changed invisible image to the 1st index of the images array. so when the page is loaded there is not a pic on the screen.

--- src/CompJS/Btn.js
+++ src/CompJS/Btn.js
@@ -12,10 +12,11 @@ function BtnDoThings() {
 */
  const [image, setImage] = useState(0);
 
- const images = [ "https://images.pexels.com/photos/101529/pexels-photo-101529.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+ const images = [ "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wcAAgMBAJRMyisAAAAASUVORK5CYII=",
+                  "https://images.pexels.com/photos/101529/pexels-photo-101529.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
                   "https://images.pexels.com/photos/2433353/pexels-photo-2433353.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 
-                  "https://images.pexels.com/photos/91216/pexels-photo-91216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-                  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wcAAgMBAJRMyisAAAAASUVORK5CYII="  ];
+                  "https://images.pexels.com/photos/91216/pexels-photo-91216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                ];
 
  const flipImageChange = (index) => {
     setImage(index);
