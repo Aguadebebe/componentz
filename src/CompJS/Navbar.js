1c8b72565915ba2c160df5204d89bba7a3fa29b6
Aguadebebe <81484212+Aguadebebe@users.noreply.github.com>
2024-12-29 02:00:16 -0500

    initial commit

---
+++ src/CompJS/Navbar.js
@@ -0,0 +1,13 @@
+import "../CompCSS/Navbar.css";
+
+function Navbar() {
+    return (
+        <div className="navbar-container"> 
+          <div className="navbar">
+           Navbar
+          </div>
+        </div>
+    );
+};
+
+export default Navbar;
